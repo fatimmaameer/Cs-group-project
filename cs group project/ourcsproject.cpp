5c88cde11f400a84ac81eec567df42d93a53801e
Fatima <fattimmaameer@gmail.com>
2024-12-12 21:42:42 +0500

    new file

--- cs group project/ourcsproject.cpp
+++ cs group project/ourcsproject.cpp
@@ -5,5 +5,25 @@
 #include<ctime>
 #include<cctype>
 struct schedule{
+   
+
+
+    string name;
+    int minage;
+    int price;
+    int time;
+    int seatinglayout;
+};
+struct reservation
+{
+    int chooseschadule;
+    int userid;
+    int age;
+    string gender;
+    int row;
+    int column;
+    int time;//time when you reserve seat;
     
-}
+};
+
+    
