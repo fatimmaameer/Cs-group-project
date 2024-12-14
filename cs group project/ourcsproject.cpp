ad8d901821777e2874eeb6f5397859287be4f000
Fatima <fattimmaameer@gmail.com>
2024-12-14 09:49:22 +0500

    resolved error

--- cs group project/ourcsproject.cpp
+++ cs group project/ourcsproject.cpp
@@ -31,13 +31,12 @@ struct reservation
 const int MAX_SCHEDULE = 100;
 const int MAX_RESERVATION = 100;
 const int MAX_ROWS = 100;
-const int MAX_COLUMN=100;
+const int MAX_COLUMN = 100;
 
-//void loadschedule(); // to be defined with paramreters (), will load the struct of the movie from the schedules file
+// void loadschedule(); // to be defined with paramreters (), will load the struct of the movie from the schedules file
 void saveschedule();
 void displaymenu();
 void managerpassword();
-
 
 int main()
 {
@@ -45,10 +44,10 @@ int main()
     return 0;
 }
 
+void displaymenu()
+{
+    int choice;
 
-void displaymenu(){
-    int  choice;
-    
     cout << "enter your designation please !\n";
     cout << "1= manager\n";
     cout << "2= operator\n";
@@ -86,7 +85,7 @@ void saveschedule()
 
         schedule s;
         cout << "Enter name: ";
-        cin.ignore(); 
+        cin.ignore();
         getline(cin, s.name);
         cout << "Enter minimum age required: ";
         cin >> s.minage;
@@ -97,47 +96,49 @@ void saveschedule()
         cout << "Enter duration in minutes: ";
         cin >> s.durationinmin;
         cout << "Enter food or snack you want in the menu: ";
-        cin.ignore(); 
+        cin.ignore();
         getline(cin, s.foodnsnacks);
-        cout << "Enter drinks you want in the menu: "; 
+        cout << "Enter drinks you want in the menu: ";
         getline(cin, s.drinks);
-        cout << "Enter seating layout: "; 
-        cin >> s.seatinglayout; 
-        outfile << s.name << " " << s.minage << " " << s.price 
-                << " " << s.time << " " << s.durationinmin 
-                << " " << s.foodnsnacks << " " << s.drinks 
+        cout << "Enter seating layout: ";
+        cin >> s.seatinglayout;
+        outfile << s.name << " " << s.minage << " " << s.price
+                << " " << s.time << " " << s.durationinmin
+                << " " << s.foodnsnacks << " " << s.drinks
                 << " " << s.seatinglayout << endl;
-                 } outfile.close();
     }
+    outfile.close();
+}
 void managerpassword()
+{
+    int password, actualpassword;
+    actualpassword = 12345;
+    string actdogname, bubble, blue, actfavclr, dogname, favclr;
+    actdogname = "bubble";
+    actfavclr = "blue";
+    cout << "enter you password ";
+    cin >> password;
+    if (password == actualpassword)
     {
-        int password, actualpassword;
-        actualpassword = 12345;
-        string actdogname, bubble,blue,actfavclr, dogname, favclr;
-        actdogname = bubble;
-        actfavclr = blue;
-        cout << "enter you password ";
-        cin >> password;
-        if (password == actualpassword)
+        cout << "carry on.\n ";
+        saveschedule();
+    }
+    else if (password != actualpassword)
+    {
+        cout << "forget password!!!!! \n ";
+        cout << "try answering these question so that we can make sure you are manager \n ";
+        cout << "your dog name\n";
+        cin >> dogname;
+        cout << "your fav clr\n";
+        cin >> favclr;
         {
-            cout << "carry on.\n ";
-             saveschedule();
-        }
-        else
-        {
-            cout << "forget password!!!!! \n ";
-            cout << "try answering these question so that we can make sure you are manager \n ";
-            cout<<"your dog name\n";
-            cin>>dogname;
-            cout<<" your fav clr\n";
-            cin>>favclr;
-            if (dogname == actdogname && favclr == actfavclr)
-            {
+            if (dogname == actdogname && favclr == actfavclr){
                 saveschedule();
-            }
-            else
-            {
-                cout << "sorry you are not manager dont waste time go and do your work";
-            }
+                }
         }
     }
+    else
+    {
+        cout << "sorry you are not manager dont waste time go and do your work";
+    }
+}
